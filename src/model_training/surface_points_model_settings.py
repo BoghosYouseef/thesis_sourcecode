859dcbd62cc1f033563199a0d77977951c9fb0ec
boghos <boghos.youseef@outlook.com>
2024-06-26 13:12:52 +0200

    fixed importing bug in surface_points_model_settings.py

--- src/model_training/surface_points_model_settings.py
+++ src/model_training/surface_points_model_settings.py
@@ -1,21 +1,143 @@
 import os
-import keras
+import keras    
 import itertools
 import numpy as np
 import pandas as pd
 import tensorflow as tf
-import keras
+from keras.callbacks import CSVLogger, ModelCheckpoint
+from pathlib import Path
 from keras import layers, initializers
 from matplotlib import pyplot as plt
 from matplotlib.ticker import MaxNLocator
 from matplotlib.offsetbox import AnchoredText
-from utils.path_funcs import get_abs_saved_models_folder_path, get_abs_path, get_abs_saved_plots_folder_path,get_abs_raw_data_folder_path
+from ..utils import utils
+from ..utils.path_funcs import *
 
-class History:
+class SurfacePointsModelForOnePatch:
+    def __init__(self, NNShape=[], diamond=False, regularizer=False, name=""):
+        self.settings = {}
+        if name:
+            try:
+                self.name = name
+                
+                model_path = get_abs_saved_surface_points_models_folder_path_with_model_name(name)
+                # full_name = str(model_path).replace("\\", " ")
+                # full_name =  full_name.replace("/", " ")
+                # full_name =  full_name.split(" ")
+                # full_name = str(full_name[-1])
+                
+                print("Model path: ", model_path)
+                # print("full_name: ", full_name)
+                # training_history_csv_path = get_patch_model_training_data_file_abs_path_by_model_name(name)
+                self.model = keras.models.load_model(model_path)
+                # colnames = ['epoch', 'loss', 'accuracy', 'val_loss', 'val_accuracy']
+                # self.history = pd.read_csv(training_history_csv_path,names=colnames, header=0)
+                self.settings["shape"] = utils.get_shape_from_name(str(model_path))
+                self.settings["shape_as_list"] = utils.get_shape_from_name_as_list(str(model_path))
+                self.settings["batch_size"] = utils.get_batch_size_from_name(str(model_path))
+                
+            except FileNotFoundError as error:
+                print(f"{error}")
+        
+        if NNShape:
+                total_layers = [layers.Input(shape=(3,))]
+                for num_of_neurons in NNShape:
+                    total_layers.append(layers.Dense(num_of_neurons, activation='relu'))
+                
+                if regularizer:
+                    total_layers.append(layers.Dense(2, activation='linear', kernel_regularizer='l2'))
+                else:
+                    total_layers.append(layers.Dense(2, activation='linear'))
+                
+                self.model = keras.Sequential(total_layers)
+                self.history = []
+                if diamond:
+                    shape_str = ""
+                    for i in NNShape:
+                        shape_str = shape_str + str(i) + "-"
+                    
+                    self.settings = {"shape":f"{shape_str}"}
 
-    def __init__(self, history, settings):
-        self.history = history
-        self.settings = settings
+                else:
+                    self.settings = {"shape":f"{num_of_neurons}-"*len(NNShape)}
+
+    def compile(self,opt, loss_, metrics_):
+        self.settings['optimizer'] = opt._name
+        self.settings['loss'] = loss_
+        self.settings['metrics'] = metrics_
+
+        self.model.compile(optimizer=opt, loss=loss_, metrics=metrics_)
+
+    def train(self, data, epochs_=5, batch_size_=64, verbose_=1, name=None):
+        X_train, X_test, Y_train, Y_test = data
+        if "epoch" not in self.settings:
+            self.settings["epoch"] = epochs_
+        if "shuffle" not in self.settings:
+            self.settings["shuffle"] = True
+        if "batch_size" not in self.settings:
+            self.settings["batch_size"] = batch_size_
+
+        if name is not None:
+            name = name+ "-" + self.__create_file_name()
+            path_training_history = Path(os.path.join(get_surface_points_model_training_data_folder_path(),name+".csv"))
+            path_to_saved_model = Path(os.path.join(get_abs_saved_models_folder_path(),name+".keras"))
+            try:
+                
+                csv_logger = CSVLogger(path_training_history)
+                model_check_point = ModelCheckpoint(path_to_saved_model)
+                self.history = self.model.fit(X_train, Y_train, epochs=epochs_, shuffle=True,batch_size=batch_size_, validation_data=(X_test, Y_test), verbose=verbose_, callbacks=[csv_logger, model_check_point])
+                # re_enumerate_epochs_in_csv_file(path_training_history)
+            except KeyboardInterrupt:
+                print("Training of model stopped!")
+                print(f"path to model: {path_to_saved_model}")
+                print(f"path to model training history: {path_training_history}")
+    
+    def retrain_existing_model(self, data, epochs_=5, batch_size_=64, verbose_=1, name=None, sample_weights=None):
+        
+        X_train, X_test, Y_train, Y_test = data
+        if "epoch" not in self.settings:
+            self.settings["epoch"] = epochs_
+        if "shuffle" not in self.settings:
+            self.settings["shuffle"] = True
+        if "batch_size" not in self.settings:
+            self.settings["batch_size"] = batch_size_
+
+        try:
+            if name is None:
+                raise NameError
+            else:
+                path_to_saved_model = get_abs_saved_surface_points_models_folder_path_with_model_name(name=name)
+                model_training_history = get_surface_points_model_training_data_file_abs_path_by_model_name(name=name)
+                
+                csv_logger = CSVLogger(model_training_history, append=True)
+                model_check_point = ModelCheckpoint(path_to_saved_model)
+                try:
+                    if sample_weights is not None:
+                        print("using sample weights " + "-#-"*10)
+                        self.history = self.model.fit(X_train, Y_train, epochs=epochs_, shuffle=True,batch_size=batch_size_, validation_data=(X_test, Y_test), verbose=verbose_, callbacks=[csv_logger, model_check_point], sample_weight=sample_weights)
+                    else:
+                        self.history = self.model.fit(X_train, Y_train, epochs=epochs_, shuffle=True,batch_size=batch_size_, validation_data=(X_test, Y_test), verbose=verbose_, callbacks=[csv_logger, model_check_point])
+
+                except KeyboardInterrupt as error:
+                    print("Training of model stopped!")
+                    print(f"path to model: {path_to_saved_model}")
+                    print(f"path to model training history: {model_training_history}")
+
+                finally:
+                    print("re-enumerating the epochs...")
+                    utils.re_enumerate_epochs_in_csv_file(model_training_history)
+    
+        except NameError as err:
+            print("name of model not found in training folder.")
+    def predict(self, points):
+        return self.model.predict(points)
+    def __create_file_name(self):
+        try:
+            print("self.settings: ", self.settings)
+            return f"shape-{self.settings['shape']}bs-{self.settings['batch_size']}"
+        
+        except AttributeError:
+            return ""
 
 
 class SurfacePointsModel:
@@ -42,25 +164,62 @@ class SurfacePointsModel:
     def load_model(self, model):
         self.model = model
 
-    def compile(self,opt, loss_, metrics_=0):
+    def compile(self,opt, loss_, metrics_=0, sample_weight=None):
         self.settings['optimizer'] = opt._name
         self.settings['loss'] = loss_
+        self.settings['metrics'] = metrics_
 
-        # self.settings['metrics'] = metrics_
+        if sample_weight is not None:
+            print("self.model = ", self.model)
+            self.model.compile(optimizer=opt, loss=loss_, metrics=metrics_, weighted_metrics=["mae"])
+        else:
+            self.model.compile(optimizer=opt, loss=loss_, metrics=metrics_)
 
-        self.model.compile(optimizer=opt, loss=loss_)#, metrics=metrics_)
-
-    def train(self, data, epochs_=5, batch_size_=64, verbose_=1):
+    def train(self, data, epochs_=5, batch_size_=64, verbose_=1, name=None, sample_weights=None):
         X_train_points,X_train_patches, X_test_points,X_test_patches, Y_train_p1, Y_train_p2, Y_test_p1,Y_test_p2 = data
-        self.history = self.model.fit({"3d-point":X_train_points,"patch": X_train_patches},{"first_point":Y_train_p1, "second_point":Y_train_p2},
-                                       epochs=epochs_, shuffle=True,batch_size=batch_size_,
-                                         validation_data=({"3d-point":X_test_points,"patch": X_test_patches}, {"first_point":Y_test_p1, "second_point":Y_test_p2}),
-                                           verbose=verbose_)
-        self.settings["epochs"] = epochs_
-        self.settings["shuffle"] = True
-        self.settings["batch_size"] = batch_size_
+        if "epoch" not in self.settings:
+            self.settings["epoch"] = epochs_
+        if "shuffle" not in self.settings:
+            self.settings["shuffle"] = True
+        if "batch_size" not in self.settings:
+            self.settings["batch_size"] = batch_size_
 
-    def predict(self, point,patch, expected_surface_points):
+        try:
+            if name is None:
+                raise NameError
+            else:
+                path_to_saved_model = get_abs_saved_surface_points_models_folder_path_with_model_name(name=name)
+                model_training_history = get_surface_points_model_training_data_file_abs_path_by_model_name(name=name)
+                
+                csv_logger = CSVLogger(model_training_history, append=True)
+                model_check_point = ModelCheckpoint(path_to_saved_model)
+                try:
+                    if sample_weights is not None:
+                        print("using sample weights " + "-#-"*10)
+                        self.history = self.model.fit({"3d-point":X_train_points,"patch": X_train_patches},{"first_point":Y_train_p1, "second_point":Y_train_p2}, epochs=epochs_, shuffle=True,batch_size=batch_size_, validation_data=({"3d-point":X_test_points,"patch": X_test_patches}, {"first_point":Y_test_p1, "second_point":Y_test_p2}), verbose=verbose_, callbacks=[csv_logger, model_check_point], sample_weight=sample_weights)
+                    else:
+                        self.history = self.model.fit({"3d-point":X_train_points,"patch": X_train_patches},{"first_point":Y_train_p1, "second_point":Y_train_p2}, epochs=epochs_, shuffle=True,batch_size=batch_size_, validation_data=({"3d-point":X_test_points,"patch": X_test_patches}, {"first_point":Y_test_p1, "second_point":Y_test_p2}), verbose=verbose_, callbacks=[csv_logger, model_check_point])
+
+                except KeyboardInterrupt as error:
+                    print("Training of model stopped!")
+                    print(f"path to model: {path_to_saved_model}")
+                    print(f"path to model training history: {model_training_history}")
+
+                finally:
+                    print("re-enumerating the epochs...")
+                    utils.re_enumerate_epochs_in_csv_file(model_training_history)
+    
+        except NameError as err:
+            print("name of model not found in training folder.")
+        # self.history = self.model.fit({"3d-point":X_train_points,"patch": X_train_patches},{"first_point":Y_train_p1, "second_point":Y_train_p2},
+        #                                epochs=epochs_, shuffle=True,batch_size=batch_size_,
+        #                                  validation_data=({"3d-point":X_test_points,"patch": X_test_patches}, {"first_point":Y_test_p1, "second_point":Y_test_p2}),
+        #                                    verbose=verbose_)
+        # self.settings["epochs"] = epochs_
+        # self.settings["shuffle"] = True
+        # self.settings["batch_size"] = batch_size_
+
+    def predict_1_point(self, point,patch, expected_surface_points):
         tensor_point = tf.convert_to_tensor([point])
         tensor_patch = tf.convert_to_tensor([patch])
         predicted_surface_points = self.model.predict({"3d-point": tensor_point,"patch":tensor_patch})
